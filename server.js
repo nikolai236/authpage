e153a04b35add6f4f59a35e73b66ff2aba313e76
piletonikolai <50989574+nikolai236@users.noreply.github.com>
2022-02-17 20:54:12 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,38 +1,45 @@
-const express = require('express');
-const cluster = require('cluster');
-const os = require('os');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require('express')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
 
-const PORT = process.env.PORT || 5000;
-const cpus = os.cpus().length;
+const https = require('https')
+const cluster = require('cluster')
+const os = require('os')
 
-// if(cluster.isPrimary || cluster.isMaster) {
+const PORT = process.env.PORT || 5000
+const cpus = os.cpus().length
 
-//     for(let i = 0; i < cpus; i++) {
-//         cluster.fork();
-//     }
+if (cluster.isPrimary || cluster.isMaster) {
+  for (let i = 0; i < cpus; i++) {
+    cluster.fork()
+  }
 
-//     cluster.on('exit', (worker, code, signal) => {
-//         if(signal) console.log(signal);
-//         if(code !== 0) console.log(code);
-//     });
+  cluster.on('exit', (worker, code, signal) => {
+    if (signal) console.log(signal)
+    if (code !== 0) console.log(code)
+  })
+} else if (cluster.isWorker) {
+  const app = express()
 
-//} else if(cluster.isWorker) {
-const app = express();
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
+  app.use(morgan('dev'))
+  app.use(cookieParser())
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+  const mainRouter = require('./routes/mainRouter')
+  const userRouter = require('./routes/userRouter')
 
-const mainRouter = require('./routes/mainRouter');
-const userRouter = require('./routes/userRouter');
+  app.use('/api/v1/main', mainRouter)
+  app.use('/api/v1/auth', userRouter)
 
-app.use('/api/v1/main', mainRouter);
-app.use('/api/v1/auth', userRouter);
-console.log(process.versions);
-app.listen(
-    PORT,
-    () => console.log('listening on ' + PORT)
-);
-//}
+  https
+    .createServer(
+      require('./ssl'),
+      app
+    )
+    .listen(
+      PORT,
+      () => console.log('listening on ' + PORT)
+    )
+}
